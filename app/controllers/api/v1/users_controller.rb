29c90af08fe76061c9af0908158ca5f153d871eb
Michael Knott <knottweb@gmail.com>
2020-10-13 10:06:04 +0100

    initial commit

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,53 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :find_user, only: [:show, :update, :destroy]
+
+  # GET /users
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  # GET /user/:id
+  def show
+    render json: @user
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render error: { error: 'Unable to create User.' }, status: 400
+    end
+  end
+
+  # PUT /users/:id
+  def update
+    if @user.update(user_params)
+      render json: { message: 'User successfully updated.' }, status: 200
+    else
+      render error: { error: 'Unable to update User.' }, status: 400
+    end
+  end
+
+  # DELETE /users/:id
+  def destroy
+    if @user
+      @user.destroy
+      render json: { message: 'User successfully deleted.' }, status: 200
+    else
+      render error: { error: 'Unable to update User.' }, status: 400
+    end
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
